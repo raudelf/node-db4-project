21b2b97c56187c05d86c9f0f530b0c77da9aea66
raudelf <rawdell@outlook.com>
2020-04-18 14:07:34 -0500

    imported my router for endpoints

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
+const CookingRouter = require('./endpoints/cooking-router.js');
+
 const server = express();
 
 server.use(express.json());
+server.use('/api', CookingRouter);
 
 module.exports = server;
